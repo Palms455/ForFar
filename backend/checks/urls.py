4c5b09992f33dc188f3258f4944f359c0d7bb9a9
Rustam Kayumov <rustrnb@yandex.ru>
2020-05-04 02:17:24 +0500

    Add base view classes

--- backend/checks/urls.py
+++ backend/checks/urls.py
@@ -1,7 +1,9 @@
 from django.conf.urls import url
+from forfar.views import GetOrder, NewCheck, PdfCheck
 
 urlpatterns = [
-
-
-
+    url(r'^create_checks/$', GetOrder.as_view(), name='create_checks'),
+    url(r'^new_checks/(?P<api_key>\w+)/$', NewCheck.as_view(), name='new_checks'),
+    url(r'^check/(?P<api_key>\w+)/(?P<check_id>\d+)/$', PdfCheck.as_view(), name='check'),
 ]
+
--- backend/checks/views.py
+++ backend/checks/views.py
@@ -1,19 +1,45 @@
-from rest_framework.decorators import api_view
+from rest_framework.views import APIView
 from django.http import JsonResponse
 from checks.models import Printer, Check
+from checks.models import CheckSerializer
+from rest_framework import status
 
 # Create your views here.
 
+class GetOrder(APIView):
+	permission_classes = (AllowAny,)
 
-@api_view(['POST'])
-def get_order(request):
-	order = json.loads(request.body.decode())
-	all_printers = Printer.objects.filter(point_id=order['point_id'])
-	if Check.objects.filter(order__id=order['id']).exists():
-		return JsonResponse({'error': "Для данного заказа уже созданы чеки"}, status=400)
-	if not all_printers:
-		return JsonResponse({'error': "Для данной точки не настроено ни одного принтера"}, status=400)
+	def post(self, request)
+		order = json.loads(request.body.decode())
+		all_printers = Printer.objects.filter(point_id=order['point_id'])
+		if Check.objects.filter(order__id=order['id']).exists():
+			return JsonResponse({'error': "Для данного заказа уже созданы чеки"}, status=400)
+		if not all_printers:
+			return JsonResponse({'error': "Для данной точки не настроено ни одного принтера"}, status=400)
+		for printer in all_printers:
+			new_check = Check.objects.create(point_id=order['point_id'])
+		return JsonResponse({'ok' : "Чеки успешно созданы"}, status=201)
 
+
+
+class NewCheck(APIView):
+	permission_classes = (AllowAny,)
+
+	def get(self, request, api_key):
+		printer = Printer.objects.get(api_key=api_key)
+		if not printer:
+			return JsonResponse({'error': "Не существует принтера с таким api_key"}, status=401)
+		checks = printer.checks_set.filter(status=Check.NEW).order_by('id')
+		list_check = CheckSerializer(checks, many=True).data
+		return JsonResponse({'checks' : list_check})
+
+class PdfCheck(APIView):
+	permission_classes = (AllowAny,)
+
+	def get(self, request, api_key, check_id):
+		if not Printer.objects.filter(api_key=api_key).exists():
+			return JsonResponse({'error': "Не существует принтера с таким api_key"}, status=401)
+		
 
 	
 
